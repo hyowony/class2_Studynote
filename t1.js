b89635561b9e874f63c634ef0c85d2ab59058caf
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-11 19:01:43 +0900

    구조분해 연습

--- t1.js
+++ t1.js
@@ -14,3 +14,23 @@ const { name, age } = id;
 
 console.log(name);
 console.log(age);
+
+const H = { num: 1, nickname: "샤크" };
+const { num, nickname } = H;
+
+console.log(num);
+console.log(nickname);
+
+///배열 구조분해 할당 코드 비교
+// 구조분해 할당을 사용하지 않을 때
+
+const games = ["토탈워", "롤", "심즈", "피파"];
+
+console.log(games[0]); //토탈워
+console.log(games[1]); // 롤
+
+//구조분해 할당 사용
+
+const [battleGround, Lol] = games;
+console.log(battleGround); //토탈워??
+console.log(Lol); //롤??
